6d3a7c61c004e666ff11d290ec74ecdb4ae56b03
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2025-06-14 19:08:33 +0700

    guardian is a deno package.

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { camelCase } from "@luca/cases";
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log(camelCase("Hello, World!"));
+  console.log("Add 2 + 3 =", add(2, 3));
+}
